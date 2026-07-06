10800699217a2778416d5c91d95ed3fd83c122ba
lzs <2468979982@qq.com>
2026-07-06 14:03:06 +0800

    update

--- src/session/types.rs
+++ src/session/types.rs
@@ -1,6 +1,7 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+pub use crate::llm::types::MessageRole;
 
 // #[derive(Debug, Clone, Serialize, Deserialize)]
 // #[serde(rename_all = "lowercase")]
